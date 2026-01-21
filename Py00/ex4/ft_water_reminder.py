53f81a1f03b27bcb00e51dec7686695f99411f5a
Nataneil Appasamy <appasamy.nataniel.an@gmail.com>
2026-01-21 19:11:49 +0100

    first module complete

---
+++ Py00/ex4/ft_water_reminder.py
@@ -0,0 +1,5 @@
+watering = int(input("Days since last watering: "))
+if watering > 2:
+    print("Water the plants!")
+else:
+    print("Plants are fine")
---
+++ Py00/ex6/ft_garden_summary.py
@@ -0,0 +1,9 @@
+
+def ft_garden_summary():
+    garden_name = input("Enter garden name: ")
+    num_plant = input("Enter a number of plants: ")
+    print(f"Garden :{garden_name}")
+    print(f"Plants: {num_plant}")
+    print("Satus: Growing well!")
+
+ft_garden_summary()
