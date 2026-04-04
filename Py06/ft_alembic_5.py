d9d562f5e4fc0abc4d3df1e621931c9e60583baa
JeanBon <appasamy.nataniel.an@gmail.com>
2026-04-04 22:15:37 +0200

    Py06 ex0 done

---
+++ Py06/ft_alembic_5.py
@@ -0,0 +1,6 @@
+from alchemy import create_air
+
+if __name__ == "__main__":
+    print("=== Alembic 3 ===")
+    print("Accessing the alchemy module using 'from alchemy import ...")
+    print(f"Testing create_air: {create_air()}")
