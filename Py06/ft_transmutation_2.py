5cbf94f98dccda61700cc373d78b111893ca565c
JeanBon <appasamy.nataniel.an@gmail.com>
2026-04-05 22:13:34 +0200

    part3 should be finished

---
+++ Py06/ft_transmutation_2.py
@@ -0,0 +1,7 @@
+import alchemy
+
+
+if __name__ == "__main__":
+    print("=== Transmutation 1 ===")
+    print("Import transmutation module directly")
+    print(f"Testing lead to gold: {alchemy.lead_to_gold()}")
