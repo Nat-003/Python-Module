c06bdead561c8927107684ab5f7b202d81fa200f
JeanBon <appasamy.nataniel.an@gmail.com>
2026-04-12 23:24:07 +0200

    started py09

---
+++ Py09/ex0/space_station.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel, Field, ValidationError
+from datetime import datetime
+from typing import Optional
+
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0.0, le=100.0)
+    oxygen_level: float = Field(ge=0.0, le=100.0)
+    last_maintenance: datetime = Field()
+    is_operational: bool = Field(default=True)
+    notes: Optional[str] = Field(max_length=200,default=None)
+
+
+
+def main() -> None:
+    print("===================")
+    print("Valid station created:")
+    station = SpaceStation(
+        station_id="ISS001",
+        name="International Space Station",
+        crew_size=6,
+        power_level=85.5,
+        oxygen_level=92.3,
+        last_maintenance="2024-06-15T10:30:00",
+        is_operational=False,
+        notes="All systems nominal"
+    )
+    print(f"ID: {station.station_id}")
+    print(f"Name: {station.name}")
+    print(f"Crew: {station.crew_size}")
+    print(f"Power: {station.power_level}")
+    print(f"Oxygene level: {station.oxygen_level}")
+    print(f"Status: {'Operational'if station.is_operational else 'Not Operational'}")
+    print()
+    print("===================")
+    print("Expected validation Error")
+    try:
+        SpaceStation(
+        station_id="X",
+        name="Bad Station",
+        crew_size=25,
+        power_level=150.0,
+        oxygen_level=-5.0,
+        last_maintenance="2024-01-01T00:00:00"
+        )
+    except ValidationError as e:
+         print(f"{e.errors()[0]['msg']}")
+
+
+if __name__ == "__main__":
+        main()
